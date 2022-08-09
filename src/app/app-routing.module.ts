b18a7c0399b01426a751a202b4fc8943398a710a
Pradeep_Yazamanam <Pradeep_Yazamanam@EPINHYDW0B31>
2022-08-09 19:13:12 +0530

    Crete Bootstrap application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'contact-list', component: ContactComponent },
+  { path: 'product-list',
+   loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+   },
+  { path: 'products', loadChildren: () =>
+   import('./products/products.module').then(m => m.ProductsModule)
+   }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
